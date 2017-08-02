d51af7d0c1204b88e4fc47694e0a253b18af086c
Tom Noogen <tom@noogen.net>
2017-08-01 23:40:06 -0500

    update conf

--- dsweb/rootfs/etc/run_always/dsweb.sh
+++ dsweb/rootfs/etc/run_always/dsweb.sh
@@ -10,24 +10,24 @@ rm -rf /var/cache/nginx/site/*
 
 chown -R nginx:nginx /var/cache/nginx
 
-# handle each env variables
-sed -i "s/ DS_RESOLVER;/ $DS_RESOLVER;/g" /usr/local/openresty/nginx/conf/nginx.conf
-sed -i "s/DS_TTL/$DS_TTL/g" /usr/local/openresty/nginx/conf/nginx.conf
-sed -i "s/DS_CONTENT/$DS_CONTENT/g" /usr/local/openresty/nginx/conf.d/zz-dsweb.inc
-sed -i "s/DS_API/$DS_API/g" /usr/local/openresty/nginx/conf.d/zz-dsweb.inc
-sed -i "s/DS_SEO/$DS_SEO/g" /usr/local/openresty/nginx/conf.d/zz-dsweb.inc
+# restore backup folder
+if [ ! -f /usr/local/openresty/nginx/conf/nginx.conf ]; then
+	rsync -a /usr/local/openresty/nginx/conf-bak/ /usr/local/openresty/nginx/conf/
 
-if [[ "$AWS_PATH" != '' ]]; then
-	# if sync to s3, download from s3 to local folder - only if folder does not exists
-	# this mean that, can manually create this folder to prevent/lock s3 restore
-	if [ ! -d /usr/local/openresty/s3/ ]; then
-		echo "[i] DSWEB restoring from s3: $AWS_PATH"
-		mkdir -p "/usr/local/openresty/s3/"
+	# handle each env variables
+	sed -i "s/ DS_RESOLVER;/ $DS_RESOLVER;/g" /usr/local/openresty/nginx/conf/nginx.conf
+	sed -i "s/DS_TTL/$DS_TTL/g" /usr/local/openresty/nginx/conf/nginx.conf
+	sed -i "s/DS_CONTENT/$DS_CONTENT/g" /usr/local/openresty/nginx/conf/conf.d/zz-dsweb.inc
+	sed -i "s/DS_API/$DS_API/g" /usr/local/openresty/nginx/conf/conf.d/zz-dsweb.inc
+	sed -i "s/DS_SEO/$DS_SEO/g" /usr/local/openresty/nginx/conf/conf.d/zz-dsweb.inc
 
-		aws s3 sync "s3://$AWS_PATH/"  "/usr/local/openresty/s3/" --exclude "*logs/*"
-		# if s3 source is valid, then overwrite local
-		if [ -f /usr/local/openresty/s3/conf/nginx.conf ]; then
-			rsync -a "/usr/local/openresty/s3/" "/usr/local/openresty/nginx/"
+	# if folder contain instruction to restore from s3 then
+	if [ -f /usr/local/openresty/nginx/conf/s3.restore ]; then
+		if [[ "$AWS_PATH" != '' ]]; then
+			echo "[i] DSWEB restoring from s3: $AWS_PATH"
+			aws s3 sync "s3://$AWS_PATH/" "/usr/local/openresty/conf/" --exclude "*logs/*"
+			
+			rm -f /usr/local/openresty/nginx/conf/s3.restore
 		fi
 	fi
 fi
